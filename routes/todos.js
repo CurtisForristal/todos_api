619ae2bba67330b1d7d9dde9064adabad4dd38e6
Curtis Forristal <curtisforristal@gmail.com>
2018-01-27 16:51:43 -0700

    added Delete route

--- routes/todos.js
+++ routes/todos.js
@@ -61,4 +61,16 @@ router.put("/:todoId", function(req, res) {
 });
 
 
+// DELETE ROUTE
+router.delete("/:todoId", function(req, res) {
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function() {
+        res.json({message: "We deleted it!"});
+    })
+    .catch(function(err) {
+        res.send(err);
+    });
+});
+
+
 module.exports = router;
