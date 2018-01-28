41d91b86c3d3a7f826afb96c57cb1e32082f3cf3
Curtis Forristal <curtisforristal@gmail.com>
2018-01-28 10:28:17 -0700

    added initial AJAX call to pull all todos from db and display them

--- public/app.js
+++ public/app.js
@@ -1,1 +1,24 @@
-alert("HI FROM APP.JS");
+
+
+// Any code inside here will not run until the DOM has loaded
+$(document).ready(function() {
+    // Get all the data from the db
+    $.getJSON("/api/todos")
+    .then(addTodos)
+    // We are not handling errors in this app, but you SHOULD!
+});
+
+
+// Adds all todos to the page
+function addTodos(todos) {
+    // Loop through each todo
+    // Append the name of each todo to the page as an li
+    todos.forEach(function(todo) {
+        // jQuery to create a new li tag
+        var newTodo = $("<li>" + todo.name + "</li>");
+        // Add the "task" class to the todo for styling
+        newTodo.addClass("task");
+        // Append it to the ul with class "list"
+        $(".list").append(newTodo);
+    });
+}
